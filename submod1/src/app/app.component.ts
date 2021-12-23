939e617690187ee1b1f5ab969f92670fb6987158
Wouter Willems <wouter@klippa.com>
2021-12-23 14:52:34 +0100

    mjeh

--- submod1/src/app/app.component.ts
+++ submod1/src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {aap} from "../../submod2/test";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'submod1';
+
+  ngOnInit(): void {
+    console.log('yay');
+    console.log(aap);
+  }
 }
